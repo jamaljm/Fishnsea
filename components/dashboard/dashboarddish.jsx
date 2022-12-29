b8a10de4c9e2ca17b90930c9ba20932692dc86c1
jam-dev <jamalvga2002@gmail.com>
2022-12-29 20:39:31 +0530

    some changes

--- components/dashboard/dashboarddish.jsx
+++ components/dashboard/dashboarddish.jsx
@@ -5,24 +5,21 @@ import Dishcard from './dishcard'
 export default function dashboard() {
   return (
       <>
-          {/* <div className='flex w-full flex-row rounded-br-xl px-5 pr-px-5 overflow-hidden bg-white'>
-              <div className='flex py-2 flex-row h-screen gap-3 overflow w-2/5 '>
+          <div className='flex flex-col gap-5 rounded-br-xl px-5 pr-px-5 bg-white'>
+              <div className='flex py-2 flex-row gap-7 mt-3 overflow '>
                   <Dishcard />
                   <Dishcard />
                   <Dishcard />
                   <Dishcard />
               </div>
-                
+                     <div className='flex py-2 flex-row gap-7 overflow '>
+                  <Dishcard />
+                  <Dishcard />
+                  <Dishcard />
+                  <Dishcard />
+              </div>
           </div>
-              <div className='flex w-full rounded-br-xl px-5 pr-px-5 overflow-hidden bg-white'>
-              <div className='flex py-2 flex-row h-screen gap-3 overflow w-2/5 '>
-                  <Dishcard />
-                  <Dishcard />
-                  <Dishcard />
-                  <Dishcard />
-              </div>
-                
-              </div> */}
+            
       </>
   )
 }
