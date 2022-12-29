b106b474abce163fab448b80c761611cadcd0452
jam-dev <jamalvga2002@gmail.com>
2022-12-29 13:15:02 +0530

    project created

---
+++ pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/navbar/navbardash'
+import Sidebar from '../components/sidebar/sidebar'
+
+export default function dashboard() {
+  return (
+      <><Navbar />
+          <Sidebar /></>
+  )
+}
---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Login from '../components/auth/Login'
+const login = () => {
+  return (
+  <Login/>
+  )
+}
+
+export default login
